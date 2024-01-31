ca83616abab06cb56a065dc71564be52503a9db1
jaanu.gudumala@gmail.com <jaanu.gudumala@gmail.com>
2024-01-31 12:56:18 +0530

    modified ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "public-instance" {
   ami = "ami-0a3c3a20c09d6f377"
   instance_type = "t2.micro"
   key_name = "Terraform-key-pair"
+  subnet_id = aws_subnet.publicsubnet.id
+  vpc_security_group_ids = [ aws_security_group.public-sg.id ]
 }
 
 
